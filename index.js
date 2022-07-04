569667fa6272f857c51a808d238f19c62ff2299f
Michael du Plessis <mdp0101@gmail.com>
2022-07-04 10:58:30 +0200

    created skeletong of backend

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
