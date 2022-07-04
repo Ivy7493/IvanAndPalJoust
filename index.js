04e09efc8d172eb47d62187d6b15c32767c41383
Michael du Plessis <mdp0101@gmail.com>
2022-07-04 13:12:20 +0200

    Created gameSocket to talk to client

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require("socket.io");
+const bodyParser = require('body-parser');
+const queueRouter = require('./backend/routes/queueRoutes.js');
+const GameSocket = require('./backend/socket/gameSocket.js');
+
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-const bodyParser = require('body-parser')
-const port = process.env.PORT || 3000
-const queueRouter = require('./backend/routes/queueRoutes.js')
+const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json({ limit: '100mb' }));
 
 app.get('/', (req, res) => {
   res.json('/Queue')
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+app.get('/Queue', queueRouter)
 
-app.get('/Queue',queueRouter)
+const socket = new GameSocket(server);
 
 server.listen(port, () => {
   console.log(`listening on ${port}`);
