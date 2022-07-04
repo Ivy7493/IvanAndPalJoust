be80bc65315ac77e30bfad432145de7667dcb6d2
Sama1am <104487445+Sama1am@users.noreply.github.com>
2022-07-04 13:10:50 +0200

    Initial route set-up and name generation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const http = require('http');
 const { Server } = require("socket.io");
 const app = express();
@@ -7,6 +8,10 @@ const io = new Server(server);
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 const queueRouter = require('./backend/routes/queueRoutes.js')
+const IndentityRouter = require('./backend/routes/identityRoutes.js')
+app.use(bodyParser.json({ limit: '100mb' }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, './backend/scripts')))
 
 
 app.get('/', (req, res) => {
@@ -18,6 +23,7 @@ io.on('connection', (socket) => {
 });
 
 app.get('/Queue',queueRouter)
+app.get('/Indentity',IndentityRouter)
 
 server.listen(port, () => {
   console.log(`listening on ${port}`);
