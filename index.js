699b9aa1b73b0fc86fcdc292cf69bc35aa94f36a
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-07-04 13:25:11 +0200

    Adds router to server frontend files

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const path = require('path')
-const http = require('http');
+const express = require("express");
+const path = require("path");
+const http = require("http");
 const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const bodyParser = require('body-parser')
-const port = process.env.PORT || 3000
-const queueRouter = require('./backend/routes/queueRoutes.js')
-const IndentityRouter = require('./backend/routes/identityRoutes.js')
-app.use(bodyParser.json({ limit: '100mb' }))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, './backend/scripts')))
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
+const queueRouter = require("./backend/routes/queueRoutes.js");
+const IndentityRouter = require("./backend/routes/identityRoutes.js");
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./backend/scripts")));
 
+app.use(express.static("frontend/thisWayUp/dist/this-way-up"));
 
-app.get('/', (req, res) => {
-  res.json('/Queue')
+app.get("/", (_, res) => {
+  res.redirect("/index.html");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (_) => {
+  console.log("a user connected");
 });
 
-app.get('/Queue',queueRouter)
-app.get('/Indentity',IndentityRouter)
+app.get("/Queue", queueRouter);
+app.get("/Indentity", IndentityRouter);
 
 server.listen(port, () => {
-  console.log(`listening on ${port}`);
+  console.log(`listening on http://localhost:${port}`);
 });
