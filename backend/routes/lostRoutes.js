5b415887b98fa4e7d92f78a096da8d882e9f18fa
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-07-05 15:13:01 +0200

    Adds lost screen to backend routes

---
+++ backend/routes/lostRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const LostRoutes = express.Router();
+
+LostRoutes.get("/", function (req, res) {
+  res.push([
+    "/scripts/player_lost_screen.js",
+    "/style.css",
+    "/scripts/api_layer.js",
+    "/scripts/navigation.js",
+    "/arrow.svg"
+  ], path.join(__dirname, '../../frontend'));
+
+  res.writeHead(200);
+  res.end(fs.readFileSync(path.join(__dirname, '../../frontend/player_lost_screen.html')));
+});
+
+
+module.exports = LostRoutes;
