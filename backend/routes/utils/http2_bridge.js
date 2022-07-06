f6be5769b1636f54aaa7bfb8e96ecadeb17b08b5
Sama1am <104487445+Sama1am@users.noreply.github.com>
2022-07-07 00:57:39 +0200

    Cleaned up non-needed files

--- backend/routes/utils/http2_bridge.js
+++
@@ -1,7 +0,0 @@
-const isHeroku = Boolean(process.env.PORT);
-
-function RunOnHttp2Only(callable) {
-  if (!isHeroku) callable();
-}
-
-module.exports = { isHeroku, RunOnHttp2Only };
--- backend/routes/waitingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const path = require("path");
-const WaitRouter = express.Router();
-
-WaitRouter.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../../frontend/waiting_for_finish.html"));
-});
-
-module.exports = WaitRouter;
