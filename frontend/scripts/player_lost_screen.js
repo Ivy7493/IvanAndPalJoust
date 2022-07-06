22462b143068648764e71dc8e78560978bcca3ec
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-07-06 09:09:22 +0200

    Fixes player lost screen to navigate

--- frontend/scripts/player_lost_screen.js
+++ frontend/scripts/player_lost_screen.js
@@ -1,52 +1,22 @@
 // This page is shown when the player has lost (or is out)
 // Here we vibrate the device for 1 second and wait for 4
 // more seconds to navigate to the waiting_for_finish_screen.
-import { getAllLostPlayers, addPlayerToLost, InitGameListener } from "./api_layer.js";
+import { InitGameListener } from "./api_layer.js";
 import { navigateTo, WAITING_FOR_FINISH } from "./navigation.js";
 
 window.onload = async function () {
   const playerId = getUrlArgument("playerId");
   InitGameListener(playerId);
 
-  const oneSecond = 400;
-  setInterval(navigateToWaitingForFinishScreen, oneSecond * 5);
-  setInterval(UpdatePlayers, oneSecond)
+  setTimeout(navigateToWaitingForFinishScreen, 1000 * 5);
 };
 
-async function UpdatePlayers(){
-  let list = await getAllLostPlayers()
-  console.log("what we got from request: ",list)
-  addPlayers(list)
-}
-
 /**
- * Called after 5 seconds after page has loaded to navigate to loading screen.
+ * Called after 5 seconds after page has loaded to navigate to waiting for finish screen.
  */
 async function navigateToWaitingForFinishScreen() {
-  navigateTo(WAITING_FOR_FINISH);
-}
-
-function vibrateDevice() {
-  // window.navigator.vibrate(300);
-}
-
-const playerList = document.querySelector(".playerList");
-
-function clearPlayersList(){
-  playerList.innerHTML = "";
-}
-
-function addPlayer(name) {
-    let newPlayer = document.createElement("div");
-    newPlayer.classList.add("playerItem");
-    newPlayer.textContent = name;
-    playerList.appendChild(newPlayer);
-}
-
-function addPlayers(list) {
-  clearPlayersList()
-  console.log("List numbers: ", list.length)
-  for(let x of list){
-    addPlayer(x)
-  }
+  const playerId = getUrlArgument("playerId");
+  navigateTo(WAITING_FOR_FINISH, {
+    playerId: playerId,
+  });
 }
