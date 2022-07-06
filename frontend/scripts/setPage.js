1d7fa1fe87dc19c0b3731406ccf35a56c203c739
Michael du Plessis <mdp0101@gmail.com>
2022-07-06 14:28:31 +0200

    git what?

---
+++ frontend/scripts/setPage.js
@@ -0,0 +1,26 @@
+import { initJoin } from "./join.js";
+import { initStart } from "./start.js";
+
+//====='PAGE' SWITCHING ON SPA=====//
+const allPages = document.querySelectorAll(".page");
+let currPage = "";
+let activePage = null;
+
+export function setPage(p) {
+    if(["game", "join", "start", "win", "lose"].indexOf(p) > -1) //Page types
+    {
+        currPage = p;
+        allPages.forEach(x => {
+            x.disabled = true; //TODO: Check
+            x.classList.add("disabled");
+        });
+        activePage = document.querySelector("." + p + "Page"); //enable current page
+        
+        if (p == "join")
+            initJoin();
+        if (p == "start")
+            initStart();
+
+        activePage.classList.remove("disabled");
+    }
+}
