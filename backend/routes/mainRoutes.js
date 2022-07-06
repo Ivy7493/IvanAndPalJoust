cc1419e45f6d890f44c9cacc9a9c6ac2a5acdbbc
Rec1dite <rec1dite@gmail.com>
2022-07-06 12:32:07 +0200

    Started move to SPA layout (see index.html)

--- backend/routes/mainRoutes.js
+++ backend/routes/mainRoutes.js
@@ -6,25 +6,26 @@ const mainRouter = express.Router();
 const { RunOnHttp2Only } = require("./utils/http2_bridge");
 
 mainRouter.get("/", function (req, res) {
-  RunOnHttp2Only(function () {
-    res.push(
-      [
-        "/scripts/join_screen.js",
-        "/join.html",
-        "/scripts/mainGame.js",
-        "/style.css",
-        "/scripts/api_layer.js",
-        "/scripts/navigation.js",
-        "/arrow.svg",
-      ],
-      path.join(__dirname, "../../frontend")
-    );
-  });
+  res.sendFile(path.join(__dirname, "../../frontend/index.html"));
+  // RunOnHttp2Only(function () {
+  //   res.push(
+  //     [
+  //       "/scripts/join_screen.js",
+  //       "/join.html",
+  //       "/scripts/mainGame.js",
+  //       "/style.css",
+  //       "/scripts/api_layer.js",
+  //       "/scripts/navigation.js",
+  //       "/arrow.svg",
+  //     ],
+  //     path.join(__dirname, "../../frontend")
+  //   );
+  // });
 
-  res.writeHead(200);
-  res.end(
-    fs.readFileSync(path.join(__dirname, "../../frontend/mainGame.html"))
-  );
+  // res.writeHead(200);
+  // res.end(
+  //   fs.readFileSync(path.join(__dirname, "../../frontend/mainGame.html"))
+  // );
 });
 
 module.exports = mainRouter;
